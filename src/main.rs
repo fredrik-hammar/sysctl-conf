b3574f46da1e2a6d0c050b5dadc7c07b086609db
Fredrik Hammar <me@fredrikhammar.se>
2023-05-13 15:18:25 +0900

    Add test with example input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use std::collections::BTreeMap;
+
 fn main() {
-    println!("Hello, world!");
+}
+
+#[allow(dead_code)]
+#[allow(clippy::needless_lifetimes)]
+fn parse<'a>(_: &'a str) -> BTreeMap<&'a str, &'a str> {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use indoc::indoc;
+
+    #[test]
+    fn test_example() {
+        let example = indoc!{r#"
+            endpoint = localhost:3000
+            debug = true
+            log.file = /var/log/console.log
+        "#};
+        assert_eq!(parse(example), BTreeMap::from([
+            ("endpoint", "localhost:3000"),
+            ("debug", "true"),
+            ("log.file", "/var/log/console.log"),
+        ]));
+    }
 }
